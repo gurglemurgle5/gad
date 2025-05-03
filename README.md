356b60b32ebde9e5e4d219cd2032006c056778ed
Gurglemurgle5 <95941332+gurglemurgle5@users.noreply.github.com>
2025-05-02 19:46:30 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+small rust program that fetches the current GAD value from https://state.corru.network, for displaying in the terminal
+
+TODO: better readme when i have more time
---
+++ src/main.rs
@@ -0,0 +1,47 @@
+use clap::Parser;
+use reqwest::blocking::Client;
+use serde::Deserialize;
+use std::cmp::Ordering;
+use std::error::Error;
+use std::time::Duration;
+
+#[derive(Debug, Parser)]
+struct Cli {
+    /// Timeout in milliseconds
+    #[arg(long, default_value_t = 3000)]
+    timeout: u64,
+}
+
+fn main() {
+    let cli = Cli::parse();
+    let result = get_gad(&cli);
+    match result {
+        Ok(gad) => {
+            // NOTE: a code of 2 is supposed to be white, but i prefer keeping it cyan
+            let color = match gad.code.cmp(&0) {
+                Ordering::Less => "\x1b[38;2;255;0;255m",    // magenta
+                Ordering::Equal => "\x1b[38;2;255;255;0m",   // yellow
+                Ordering::Greater => "\x1b[38;2;0;255;255m", // cyan
+            };
+            println!("{color}GAD::'{}'\x1b[0m", gad.status);
+        }
+        Err(err) => {
+            println!("\x1b[38;2;255;0;0mERROR::'{err}'");
+        }
+    }
+}
+
+#[derive(Deserialize)]
+struct GadState {
+    code: i32,
+    status: String,
+}
+
+fn get_gad(cli: &Cli) -> Result<GadState, Box<dyn Error>> {
+    let client = Client::builder()
+        .timeout(Duration::from_millis(cli.timeout))
+        .build()?;
+    let text = client.get("https://state.corru.network").send()?.text()?;
+    let gad = serde_json::from_str(&text)?;
+    Ok(gad)
+}
